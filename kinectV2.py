b5fac6abccf7904bd72e9ae821e15c1a16c59c52
dev-pa5an <152805932+dev-pa5an@users.noreply.github.com>
2024-03-03 16:31:51 +0530

    Update kinectV2.py

--- kinectV2.py
+++ kinectV2.py
@@ -1,53 +1,31 @@
 import cv2
-from darkflow.net.build import TFNet
 from pykinect2 import PyKinectV2
 from pykinect2.PyKinectV2 import *
 from pykinect2 import PyKinectRuntime
 import numpy as np
 import time
-import tensorflow as tf
 
-
-config = tf.ConfigProto(log_device_placement=True)
-config.gpu_options.allow_growth = True
-with tf.Session(config=config) as sess:
-    options = {
-            'model': 'cfg/yolov2-tiny-voc.cfg',
-            'load': 'bin/yolov2-tiny-voc.weights',
-            'threshold': 0.2,
-            'gpu': 0.3
-                    }
-    tfnet = TFNet(options)
-
-colors = [tuple(255 * np.random.rand(3)) for _ in range(10)]
-kinect = PyKinectRuntime.PyKinectRuntime(PyKinectV2.FrameSourceTypes_Color)
 kinectD = PyKinectRuntime.PyKinectRuntime(PyKinectV2.FrameSourceTypes_Depth)
-
 
 while True:
     stime = time.time()
-    if kinect.has_new_color_frame():
-        frame = kinect.get_last_color_frame()
-        frame = np.reshape(frame, (1080, 1920, 4))
-        frame = cv2.cvtColor(frame, cv2.COLOR_RGBA2RGB)
-        frame = cv2.resize(frame, (0, 0), fx=0.5, fy=0.5)
-        results = tfnet.return_predict(frame)
-        for color, result in zip(colors, results):
-            tl = (result['topleft']['x'], result['topleft']['y'])
-            #cnt = (result['x'], result['y'])
-            br = (result['bottomright']['x'], result['bottomright']['y'])
-            label = result['label']
-            confidence = result['confidence']
-            text = '{}: {:.0f}%'.format(label, confidence * 100)
-            frame = cv2.rectangle(frame, tl, br, color, 5)
-            frame = cv2.putText(
-                frame, text, tl, cv2.FONT_HERSHEY_COMPLEX, 1, (0, 0, 0), 2)
-        cv2.imshow('frame', frame)
-        frame = None
-        #print('FPS {:.1f}'.format(1 / (time.time() - stime)))
-        print(tl)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
 
-#frame.release()
+    if kinectD.has_new_depth_frame():
+        frameD = kinectD.get_last_depth_frame()
+        frameDepth = kinectD._depth_frame_data
+        frameD = frameD.astype(np.uint8)
+        frameD = np.reshape(frameD,(424, 512))
+        frameD = cv2.cvtColor(frameD, cv2.COLOR_GRAY2BGR)
+
+        def click_event(event, x, y, flags, param):
+            if event == cv2.EVENT_LBUTTONDOWN:
+                Pixel_Depth = frameDepth[(((y - 1) * 512) + x)]
+                print(Pixel_Depth)
+
+        cv2.imshow('frameD', frameD)
+        cv2.setMouseCallback('frameD', click_event)
+        
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
 cv2.destroyAllWindows()
